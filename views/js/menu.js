9d5e05948b948d995f14ad5ae86a3d02da13cbe6
Romulo <romulo.ufsc@gmail.com>
2022-08-24 16:06:30 +0200

    GPY020-159 Create admin log pag

---
+++ views/js/menu.js
@@ -0,0 +1,38 @@
+/**
+ * Admin menu JS
+ *
+ * @package   prestashop GoPay gateway
+ * @author    GoPay
+ * @link      https://www.gopay.com/
+ * @copyright 2022 GoPay
+ * @since     1.0.0
+ * @version   1.0.0
+ */
+
+/**
+ * Open Pop Up log
+ *
+ * Version: 1.0.0
+ */
+function openPopup(log) {
+	const popup = document.getElementById( 'prestashop-gopay-menu-popup' );
+	popup.querySelectorAll( 'span' ).forEach( e => e.remove() );
+	const span     = document.createElement( 'span' );
+	span.innerHTML = JSON.stringify( log );
+	popup.append( span );
+
+	popup.style.display = 'grid';
+}
+
+/**
+ * Close Pop Up log
+ *
+ * Version: 1.0.0
+ */
+function closePopup() {
+	const popup = document.getElementById( 'prestashop-gopay-menu-popup' );
+	const elem  = popup.querySelectorAll( 'span' );
+	elem.forEach( e => e.remove() );
+
+	popup.style.display = 'none';
+}
