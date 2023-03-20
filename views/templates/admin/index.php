effd7de9cc22045eb72c034da7d53a910f09ccc6
Tomas Zeman <tomaszeman@Tomass-MacBook-Pro.local>
2023-03-20 08:43:09 +0100

    GPY023-7: index files were added through autoindex

---
+++ views/templates/admin/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author    GoPay
+ * @copyright 2022 GoPay
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html  GPLv2 or later
+ * @link      https://www.gopay.com/
+ * @package   PrestaShop GoPay gateway
+ * @since     1.0.0
+ */
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+
+header('Location: ../');
+exit;
